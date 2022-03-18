7bf52fd79035d255d60e1b2d42ae5af81df8a615
angelina <angelina@aliceadsl.fr>
2022-03-18 18:56:23 +0100

    some progress in the code

--- Extractor_to_csv.py
+++ Extractor_to_csv.py
@@ -3,20 +3,35 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import regex as re
 
-link = 'https://en.wikipedia.org/wiki/Comparison_of_digital_SLRs'
-linkBis = 'https://en.wikipedia.org/wiki/List_of_most_visited_websites' 
-response = requests.get(link) #request which gets the url's encoded content
-soup = BeautifulSoup(response.text,'html.parser') #Scrape Webpage. reponse.text : url's decoded content
-#print(soup.prettify()) # To see the html code of the Wikipedia page
+link = 'https://en.wikipedia.org/wiki/Comparison_of_digital_SLRs' 
 
-#To create a text file temp in which one can save the html code of the Wikipedia page
-#fichier = open("temp.txt", "w")
-#fichier.write(soup.prettify())
-#fichier.close()
+def getAllRefs(url = str) :
+
+    response = requests.get(url) #request which gets the url's encoded content
+    soup = BeautifulSoup(response.text,'html.parser') #scrape Webpage. reponse.text : url's decoded content
+    #print(soup.prettify()) # To see the html code of the Wikipedia page
+
+    httpsrefs = {}
+
+    # Get all the references in the body of the html code
+    allrefs = soup.find(id="bodyContent").find_all(rel=True) #find all tags which have the attribute "rel"
+    #print(allrefs)
+
+    for ref in allrefs: 
+        if ref['rel'] == ['nofollow'] : # We are only interested in https pages and not wiki articles
+            httpsrefs[allrefs.index(ref)] = ref.get('href')
+        #try :
+            #temp = re.search("(?P<url>https?://[^\s]+)", str(ref)).group("url")
+            #httpsrefs[allrefs.index(ref)] = temp
+        #except AttributeError :
+            #pass
+    for key in list(httpsrefs) :
+        if key >129 :
+            httpsrefs.pop(key)
+    return(httpsrefs, len(httpsrefs)) #length must be 130
+
 
 def wikiTable_to_csv(url = str):
-    httpsrefs = {}
-    L =[]
     tableName  = url.split("/")[-1]
     tables = pd.read_html(url, header=0) #reads html tables into a list of dataframe objects
     #print(tables)
@@ -25,23 +40,9 @@ def wikiTable_to_csv(url = str):
     if not tables[1].empty:
         dataframeName = tableName + " table.csv"
 
-        # Get all the references in the body of the html code
-        allrefs = soup.find(id="bodyContent").find_all("a")
-        #print(allrefs)
-
-        condition1 = "/wiki/"
-        condition2 = "/w/"
-        #condition3 = "#"
-        for ref in allrefs: 
-            if condition1 not in str(ref) and condition2 not in str(ref) : #and condition3 not in str(ref) : # We are only interested in https pages and not wiki articles
-                httpsrefs[allrefs.index(ref)] = ref.get('href')
-                #temp = re.search("(?P<url>https?://[^\s]+)", string).group("url")
-                #httpsrefs[L.index(string)] = temp
-        print(httpsrefs)
-        print(len(httpsrefs)) #must be around 141
         tables[1].to_csv(dataframeName, sep=',') 
 
-result = wikiTable_to_csv(link)
-print(result)
-
-#<a rel="nofollow" class="external text" href="https://www.optyczne.pl/895-Panasonic_Lumix_DMC-GH1-specyfikacja_aparatu.html">[1]</a>
+intermediate = getAllRefs(link)
+print(intermediate)
+#result = wikiTable_to_csv(link)
+#print(result)

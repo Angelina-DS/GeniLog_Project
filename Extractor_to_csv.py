e7c9fbcf404f1bc63ee93d1c586bb434cadf2c59
angelina <angelina@aliceadsl.fr>
2022-03-20 14:01:14 +0100

    commit d'appoint

--- Extractor_to_csv.py
+++ Extractor_to_csv.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import regex as re
+import pprint
 
 link = 'https://en.wikipedia.org/wiki/Comparison_of_digital_SLRs' 
 
@@ -18,17 +19,25 @@ def getAllRefs(url = str) :
     #print(allrefs)
 
     for ref in allrefs: 
+        string = str(ref)
+        element = "["
+        if element in string :
+            id = string[string.index(element) + 1]
         if ref['rel'] == ['nofollow'] : # We are only interested in https pages and not wiki articles
-            httpsrefs[allrefs.index(ref)] = ref.get('href')
+            if id not in httpsrefs :
+                httpsrefs[id] = [ref.get('href')]
+            else :
+                httpsrefs[id].append(ref.get('href'))
         #try :
             #temp = re.search("(?P<url>https?://[^\s]+)", str(ref)).group("url")
             #httpsrefs[allrefs.index(ref)] = temp
         #except AttributeError :
             #pass
-    for key in list(httpsrefs) :
-        if key >129 :
-            httpsrefs.pop(key)
-    return(httpsrefs, len(httpsrefs)) #length must be 130
+    #for key in list(httpsrefs) : #we deleted urls that are not in the table
+        #if key > 129 :
+            #httpsrefs.pop(key)
+    pprint.pprint(httpsrefs)
+    return(len(httpsrefs)) #length must be 130
 
 
 def wikiTable_to_csv(url = str):
